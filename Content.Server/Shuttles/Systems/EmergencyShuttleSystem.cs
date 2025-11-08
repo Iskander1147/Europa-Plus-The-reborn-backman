ff2357b7bf0892796d608478608c47c76df8c8c1
UmbiMax <109347499+UmbiMax@users.noreply.github.com>
2025-11-08 16:20:54 +0500

    Update EmergencyShuttleSystem.cs

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
@@ -61,7 +61,7 @@
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
-//
+// SPDX-FileCopyrightText: 2025 Umbi_Max <maxim1213141@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.Linq;
